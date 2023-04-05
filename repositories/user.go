96183a16ea8ba55b341a49824fa8ea17b27db145
mhmmdazmye@gmail.com <mhmmdazmye@gmail.com>
2023-04-05 11:13:02 +0700

    creating rest api with go echo framework using gorm framework

--- repositories/user.go
+++ repositories/user.go
@@ -2,7 +2,6 @@ package repositories
 
 import (
 	"dumbflix/models"
-	"time"
 
 	"gorm.io/gorm"
 )
@@ -11,8 +10,8 @@ type UserRepository interface {
 	FindUsers() ([]models.Users, error)
 	GetUser(ID int) (models.Users, error)
 	CreateUser(user models.Users) (models.Users, error)
-	UpdateUser(user models.Users, ID int) (models.Users, error)
-	DeleteUser(user models.Users, ID int) (models.Users, error)
+	UpdateUser(user models.Users) (models.Users, error)
+	DeleteUser(user models.Users) (models.Users, error)
 }
 
 type repository struct {
@@ -25,30 +24,30 @@ func RepositoryUser(db *gorm.DB) *repository {
 
 func (r *repository) FindUsers() ([]models.Users, error) {
 	var users []models.Users
-	err := r.db.Raw("SELECT * FROM users").Scan(&users).Error
+	err := r.db.Find(&users).Error
 	return users, err
 }
 
 func (r *repository) GetUser(ID int) (models.Users, error) {
 	var user models.Users
-	err := r.db.Raw("SELECT * FROM users WHERE id=?", ID).Scan(&user).Error
+	err := r.db.First(&user, ID).Error
 	return user, err
 }
 
 func (r *repository) CreateUser(user models.Users) (models.Users, error) {
-	err := r.db.Exec("INSERT INTO users(name,email,password,created_at, updated_at) VALUES (?,?,?,?,?)", user.Name, user.Email, user.Password, time.Now(), time.Now()).Error
+	err := r.db.Create(&user).Error
 
 	return user, err
 }
 
-func (r *repository) UpdateUser(user models.Users, ID int) (models.Users, error) {
-	err := r.db.Raw("UPDATE users SET name=?, email=?, password=?, updated_at=? WHERE id=?", user.Name, user.Email, user.Password, time.Now(), ID).Scan(&user).Error
+func (r *repository) UpdateUser(user models.Users) (models.Users, error) {
+	err := r.db.Save(&user).Error
 
 	return user, err
 }
 
-func (r *repository) DeleteUser(user models.Users, ID int) (models.Users, error) {
-	err := r.db.Raw("DELETE FROM users WHERE id=?", ID).Scan(&user).Error
+func (r *repository) DeleteUser(user models.Users) (models.Users, error) {
+	err := r.db.Delete(&user).Scan(&user).Error
 
 	return user, err
 }
